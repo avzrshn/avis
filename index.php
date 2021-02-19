90eef5e635c71e8447991c0d5ed9cf8cd7d7439b
avzrshn <79317976+avzrshn@users.noreply.github.com>
2021-02-19 20:18:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK SUZURAN</title>
+</head>
+
+<body>
+    <header>
+        <center><h1>PENDAFTARAN SISWA BARU</h1></center>
+        <center><h2>SMK SUZURAN</h2></center>
+		<center><h3>Jl. Thoarushi, Distrik 69, nomer 123</h3></center>
+    </header>
+    <nav>
+        <ul>
+            <pre><a href="form-daftar.php"><h2><font color=blue>Daftar Baru</color></a>     <a href="list-siswa.php"><font color=blue>Pendaftar</color></a></h2>
+        </ul>
+    </nav>
+    </body>
+</html>
